6b47342479331b7a04187adb571e23510714e700
Airray117 <62633742+Airray117@users.noreply.github.com>
2024-12-15 01:00:08 -0800

    Fifth commit

--- CSC 680 Public Transit/ContentView.swift
+++ CSC 680 Public Transit/ContentView.swift
@@ -1,53 +1,67 @@
 import SwiftUI
 import CoreLocation
 
-struct ContentView: View {
+struct ContentView: View
+{
     @StateObject private var manager = MuniTransitManager()
     @State private var locationPermissionDenied = false
     @State private var selectedPinLocation: CLLocationCoordinate2D = CLLocationCoordinate2D()
     
-    var body: some View {
-        TabView {
-            // Map Tab with Pin Selection
-            ZStack {
+    var body: some View
+    {
+        TabView
+        {
+            ZStack
+            {
                 MapWithPinView(pinLocation: $selectedPinLocation)
                     .onChange(of: selectedPinLocation) { newValue in
                         // Update manager with selected location
-                        if let safeLocation = Optional(newValue) {
+                        if let safeLocation = Optional(newValue)
+                        {
                             manager.fetchNearestTransitStop(location: safeLocation)
-                        } else {
+                        }
+                        else
+                        {
                             print("Error: Pin location is nil!")
                         }
                     }
             }
-            .tabItem {
+            .tabItem
+            {
                 Label("Pick Location", systemImage: "map")
             }
             
-            // Muni Stops Tab
             MuniArrivalsView(manager: manager)
-                .tabItem {
-                    Label("Muni Stops", systemImage: "tram.fill")
-                }
-                .onAppear {
-                    checkLocationPermission()
-                }
+            .tabItem
+            {Label("Muni Stops", systemImage: "tram.fill")}
+            .onAppear
+            {
+                checkLocationPermission()
+            }
+            
+            BlackjackGameView()
+            .tabItem
+            {Label("Blackjack", systemImage: "suit.club.fill")}
         }
     }
     
-    // MARK: - Check Location Permission
-    private func checkLocationPermission() {
-        if CLLocationManager.authorizationStatus() == .denied {
+    private func checkLocationPermission()
+    {
+        if CLLocationManager.authorizationStatus() == .denied
+        {
             locationPermissionDenied = true
-        } else {
-            manager.requestLocationAuthorization() // Ensures location permission is requested
+        }
+        else
+        {
+            manager.requestLocationAuthorization()
         }
     }
 }
 
-// MARK: - Preview
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ContentView()
     }
 }
